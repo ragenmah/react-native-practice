75318ef2e064845b713515d3fc128134bc225e73
Ragen Maharjan <ragenmah99@gmail.com>
2022-02-08 13:51:15 +0545

    Organized the folder directory

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
-import Calculator from './components/Calculator';
+import Calculator from './src/Calculator';
 
 const App = () => {
   return (
